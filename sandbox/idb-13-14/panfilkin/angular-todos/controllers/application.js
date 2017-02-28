34c770d1c80491c1774bff838c64230405874e3f
Alex Panfilkin <soild@mail.ru>
2017-02-28 10:37:53 +0300

    Update application.js

--- sandbox/idb-13-14/panfilkin/angular-todos/controllers/application.js
+++ sandbox/idb-13-14/panfilkin/angular-todos/controllers/application.js
@@ -3,7 +3,7 @@ var app = angular.module('application',['ngRoute','ngResource'])
 app.config(function($routeProvider){
 	$routeProvider
 	.when('/', {
-		templateUrl: '/view/todos.html',
+		templateUrl: 'view/todos.html',
 		controller: 'todo-list'
 	})
 	.otherwise({
