ac9caf0ae37ddde96b830b49e5d5327fa2c25d63
Dmitriy Larionov <foldlord@yandex.ru>
2017-03-06 22:36:13 +0300

    Add files via upload

---
+++ sandbox/idb-13-14/FoldLord/Modul_1/js/js-padding.js
@@ -0,0 +1,6 @@
+function ChangePadding () {
+	            var myDiv = document.getElementById ("myDiv");
+	            var paddingInput = document.getElementById ("paddingInput");
+
+	            myDiv.style.padding = paddingInput.value + "px";
+	        }
