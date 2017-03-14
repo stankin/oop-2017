f2c1a826767e27eacda519f07e81bcf858b14f18
Sergii <counterdisclaimer@gmail.com>
2017-03-14 20:40:06 +0300

    Add files via upload

---
+++ sandbox/idb-13-12/Gorbunov/index.js
@@ -0,0 +1,33 @@
+$("#spa-btn").click(function(){
+	$("#spa").slideToggle();
+});
+$("#div-btn").click(function(){
+	if($(".page-header").hasClass("d1")){
+		$(".page-header").removeClass("d1");
+	}else{
+		$(".page-header").addClass("d1");
+	}
+	if($(".panel-default").hasClass("d1")){
+		$(".panel-default").removeClass("d1");
+	}else{
+		$(".panel-default").addClass("d1");
+	}
+	if($(".panel-heading").hasClass("d2")){
+		$(".panel-heading").removeClass("d2");
+	}else{
+		$(".panel-heading").addClass("d2");
+	}
+	if($(".panel-body").hasClass("d3")){
+		$(".panel-body").removeClass("d3");
+	}else{
+		$(".panel-body").addClass("d3");
+	}
+	if($(".text-center").hasClass("d4")){
+		$(".text-center").removeClass("d4");
+	}else{
+		$(".text-center").addClass("d4");
+	}
+});
+$("#diag-btn").click(function(){
+	$("#diag").slideToggle();
+});
