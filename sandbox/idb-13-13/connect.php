759946bec2e078dc6f7780afb856f7a4a4b96034
skiks <skiks63@gmail.com>
2017-03-16 11:03:50 +0300

    Update connect.php

--- sandbox/idb-13-13/connect.php
+++ sandbox/idb-13-13/connect.php
@@ -4,6 +4,7 @@ $dbname = "gb_oop";
 $dbuser = "gb_oop";
 $dbpasswd = "b4b92bz6nmz";
 $dbcnx = @mysql_connect($dblocation,$dbuser,$dbpasswd);
+echo 123;
 if (!$dbcnx) 
 {
   echo( "<P>В настоящий момент сервер базы данных недоступен, поэтому 
