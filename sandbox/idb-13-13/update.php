9240b415cdb38f269525705552a8c319497fe8fe
skiks <skiks63@gmail.com>
2017-03-16 14:31:30 +0300

    Update update.php

--- sandbox/idb-13-13/update.php
+++ sandbox/idb-13-13/update.php
@@ -14,7 +14,7 @@ foreach($parm as $k=>$v) {
 	$param=substr($k, 0, 2);
 	$value=trim(stripslashes(htmlspecialchars($v)));
 	$id=substr($k, 2, 2);
-	if (!empty($value))
+	if ((!empty($value))AND($k!=="pass"))
 	{
 		$sqlstring="UPDATE idb1313 SET ".$param."=".$value." where id=".$id;
 		$sql= mysql_query($sqlstring);
