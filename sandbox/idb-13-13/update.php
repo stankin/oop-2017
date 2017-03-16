290739402af63912b9c94a94606051eee2a7ecd4
skiks <skiks63@gmail.com>
2017-03-16 13:54:28 +0300

    Update update.php

--- sandbox/idb-13-13/update.php
+++ sandbox/idb-13-13/update.php
@@ -7,10 +7,15 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 	echo('Метод GET ('.count($parm).'):<br>');
 }
 foreach($parm as $k=>$v) {
+	$param=substr($k, 0, 2);
+	$value=trim(stripslashes(htmlspecialchars($v)));
+	$id=substr($k, 2, 2);
+	$sql= mysql_query("UPDATE idb1313 SET ",$param," = ",$value," where id = ",$id,"");
+	echo "UPDATE idb1313 SET ",$param," = ",$value," where id = ",$id,"";
 	echo('<b>'.$k.'</b> = '.trim(stripslashes(htmlspecialchars($v))).'<br>');
 }
 include('connect.php');
 //$_POST['id']=
-//$sql= mysql_query("UPDATE idb1313 SET M1 = '".$_POST['M1',$_POST['id']]."', M2 = '".$_POST['M2',$_POST['id']]."' where id = ".$_POST['id']."");
+//$sql= mysql_query("UPDATE idb1313 SET M1 = '', M2 = '' where id = "");
 	//echo "UPDATE idb1313 SET M1 = '".$_POST['M1',$_POST['id']]."', M2 = '".$_POST['M2',$_POST['id']]."' where id = ".$_POST['id']."";
 ?>
