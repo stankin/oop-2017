addcc2daf92904a5f36fc73a9e899305cf39852e
skiks <skiks63@gmail.com>
2017-03-16 13:28:50 +0300

    Update update.php

--- sandbox/idb-13-13/update.php
+++ sandbox/idb-13-13/update.php
@@ -9,4 +9,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 foreach($parm as $k=>$v) {
 	echo('<b>'.$k.'</b> = '.trim(stripslashes(htmlspecialchars($v))).'<br>');
 }
+include('connect.php');
+$sql= mysql_query("UPDATE idb1313 SET M1 = '".$_POST['M1']."', M2 = '".$_POST['M2']."' where id = ".$_POST['id']."");
+	
 ?>
