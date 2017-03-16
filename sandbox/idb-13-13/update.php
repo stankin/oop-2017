1aaad51ad9bebf036374121a38464f6d5063b782
skiks <skiks63@gmail.com>
2017-03-16 11:27:55 +0300

    Update update.php

--- sandbox/idb-13-13/update.php
+++ sandbox/idb-13-13/update.php
@@ -1,6 +1,7 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 	$parm = $_POST;
+	echo('Метод POST ('.count($parm).'):<br>');
 } else {
 	$parm = $_GET;
 	echo('Метод GET ('.count($parm).'):<br>');
