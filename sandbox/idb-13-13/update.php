c05ebd029bf2e9333391f9179571a6313b294975
skiks <skiks63@gmail.com>
2017-03-16 13:39:47 +0300

    Update update.php

--- sandbox/idb-13-13/update.php
+++ sandbox/idb-13-13/update.php
@@ -10,6 +10,7 @@ foreach($parm as $k=>$v) {
 	echo('<b>'.$k.'</b> = '.trim(stripslashes(htmlspecialchars($v))).'<br>');
 }
 include('connect.php');
-$sql= mysql_query("UPDATE idb1313 SET M1 = '".$_POST['M1']."', M2 = '".$_POST['M2']."' where id = ".$_POST['id']."");
-	echo "UPDATE idb1313 SET M1 = '".$_POST['M1']."', M2 = '".$_POST['M2']."' where id = ".$_POST['id']."";
+
+$sql= mysql_query("UPDATE idb1313 SET M1 = '".$_POST['M1',$_POST['id']]."', M2 = '".$_POST['M2',$_POST['id']]."' where id = ".$_POST['id']."");
+	echo "UPDATE idb1313 SET M1 = '".$_POST['M1',$_POST['id']]."', M2 = '".$_POST['M2',$_POST['id']]."' where id = ".$_POST['id']."";
 ?>
