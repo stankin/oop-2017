512b1dd5f01bfadbf25dacb5836317eb6a696019
skiks <skiks63@gmail.com>
2017-03-16 18:36:30 +0300

    Update update.php

--- sandbox/idb-13-13/update.php
+++ sandbox/idb-13-13/update.php
@@ -9,20 +9,32 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 	echo('Метод GET ('.count($parm).'):<br>');
 }
 if(md5($_POST['pass'])=="865b02aab501e77c8ca524c9bc1cf5c4")
-   {
-foreach($parm as $k=>$v) {
-	$param=substr($k, 0, 2);
-	$value=trim(stripslashes(htmlspecialchars($v)));
-	$id=substr($k, 2, 2);
-	if ((!empty($value))AND($k!=="pass"))
-	{
-		$sqlstring="UPDATE idb1313 SET ".$param."=".$value." where id=".$id;
-		$sql= mysql_query($sqlstring);
+{
+	foreach($parm as $k=>$v) {
+		$param=substr($k, 0, 2);
+		$value=trim(stripslashes(htmlspecialchars($v)));
+		$id=substr($k, 2, 2);
+		if ((!empty($value))AND($k!=="pass"))
+		{
+			$sqlstring="UPDATE idb1313 SET ".$param."=".$value." where id=".$id;
+			$sql= mysql_query($sqlstring);
+		}
+
 	}
-	
-}
 	echo('Информация сохранена!');
-   }
+	http://paul.1gb.ru/stankin/oop/sandbox/idb-13-13/
+}
 else
 	echo "Не верный пароль!";
+			$name_of_uploaded_file=$_FILES["userfile"]["name"];
+			$upload_folder='files/';
+			$path_of_uploaded_file = "http://api.screenshotmachine.com/?key=76a292&url=http://paul.1gb.ru/stankin/oop/sandbox/idb-13-13/",$row['link'];
+			$tmp_path = $_FILES["userfile"]["tmp_name"];
+			if(is_uploaded_file($tmp_path))
+			{
+				if(!copy($tmp_path,$path_of_uploaded_file))
+				{
+				$errors .= '\n error while copying the uploaded file';
+				}
+			}
 ?>
