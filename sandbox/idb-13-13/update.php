9b1a9f3798197535603471a32544f7e9cdb953e9
skiks <skiks63@gmail.com>
2017-03-16 14:38:42 +0300

    Update update.php

--- sandbox/idb-13-13/update.php
+++ sandbox/idb-13-13/update.php
@@ -3,12 +3,12 @@
 include('connect.php');
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 	$parm = $_POST;
-	echo('Информация сохранена<br>');
+	
 } else {
 	$parm = $_GET;
 	echo('Метод GET ('.count($parm).'):<br>');
 }
-if($_POST['pass']=="findme")
+if(md5($_POST['pass'])=="865b02aab501e77c8ca524c9bc1cf5c4")
    {
 foreach($parm as $k=>$v) {
 	$param=substr($k, 0, 2);
@@ -21,6 +21,7 @@ foreach($parm as $k=>$v) {
 	}
 	
 }
+	echo('Информация сохранена!');
    }
 else
 	echo "Не верный пароль!";
