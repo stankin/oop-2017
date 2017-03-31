83052fd4a956b5b3507e70e12fbd8fe166315a3a
Dmitry Kochev <kochevd@gmail.com>
2017-03-31 03:52:00 +0300

    Resolved caching issue

--- sandbox/idb-13-12/dmitry/init.php
+++ sandbox/idb-13-12/dmitry/init.php
@@ -1,5 +1,9 @@
 <?
 //error_reporting(0);
+header('Expires: Sun, 01 Jan 2014 00:00:00 GMT');
+header('Cache-Control: no-store, no-cache, must-revalidate');
+header('Cache-Control: post-check=0, pre-check=0', FALSE);
+header('Pragma: no-cache');
 include_once "libs/godb.php";
 include_once "libs/vk.php";
 
