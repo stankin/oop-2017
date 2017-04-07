27e95ea148a917572c8370973fbd789a8d454d28
KuzminVi <chelodare@gmail.com>
2017-04-07 23:17:45 +0400

    Update spa.js

--- sandbox/idb-13-12/KuzminVi/spa.js
+++ sandbox/idb-13-12/KuzminVi/spa.js
@@ -22,8 +22,8 @@ function updateClock()
 ctx = canvas.getContext("2d");
 radius = canvas.height / 2;
 ctx.translate(radius, radius);
-radius = radius * 0.90;
-setInterval(drawClock, 1000);
+radius = radius * 0.90; 
+setInterval( function() { drawClock(ctx,radius); }, 1000 );
 drawClock(ctx,radius);
  }
 function drawClock(ctx,radius) {
