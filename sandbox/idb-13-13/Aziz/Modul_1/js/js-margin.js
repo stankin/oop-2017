e3482e5625cf9ca63b3ac6c5b458470916419d29
okoff <p.ovchinnikov@STANKINORG.local>
2017-04-07 13:17:25 +0300

    Пакетная загрузка модуля 2

---
+++ sandbox/idb-13-13/Aziz/Modul_1/js/js-margin.js
@@ -0,0 +1,5 @@
+function ChangeMargin () {
+	            var myDiv = document.getElementById ("myDiv");
+	            var marginInput = document.getElementById ("marginInput");
+	            myDiv.style.margin = marginInput.value + "px";
+	        }
