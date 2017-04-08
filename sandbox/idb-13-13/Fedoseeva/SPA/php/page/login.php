5647c7d296f2762d31b8cf2baf106041e42e6b87
Alexandra Fedoseeva <alexandra_fedoseeva@mail.ru>
2017-04-08 10:09:33 +0300

    Add files via upload

---
+++ sandbox/idb-13-13/Fedoseeva/SPA/php/page/login.php
@@ -0,0 +1,31 @@
+<?php 
+$content = '<div class="screen login">
+			<div class="login__logo">
+				<h1 class="logo__header">Chat</h1>
+				<div class="logo__icon">
+					<div class="icon">
+						<span class="icon__line icon__line_one"></span>
+						<span class="icon__line icon__line_two"></span>
+					</div>
+				</div>				
+			</div>
+			<div class="login__content">
+				<div class="form">
+					<div class="form__data">
+						<span>username</span>
+						<div class="form__input">
+							<input type="text" id="user">
+						</div>
+						<span>password</span>
+						<div class="form__input">
+							<input type="password">
+						</div>
+					</div>					
+					<div class="form__button">
+						<span></span>
+						<span>Get Started</span>
+					</div>	
+				</div>
+			</div>
+		</div>';
+ ?>
